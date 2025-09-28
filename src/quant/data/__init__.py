3aecee3ea08691a4023e66bdf62444122f54bf09
Abolfazl Karimi <25490389+abol-karimi@users.noreply.github.com>
2025-09-28 15:07:33 -0400

    Separate notebook for downloading and storing the data.

--- src/quant/data/__init__.py
+++ src/quant/data/__init__.py
@@ -3,14 +3,14 @@ import pandas as pd
 import quant.data.interface.binance as binance
 
 
-def get_returns(tickers, freq, start_ts, end_ts, interface='binance'):
+def download_returns(tickers, freq, start_ts, end_ts, interface='binance'):
     if interface == 'binance':
-        return get_returns_binance(tickers, freq, start_ts, end_ts)
+        return download_returns_binance(tickers, freq, start_ts, end_ts)
     else:
         raise NotImplementedError(f'Interface {interface} is not implemented!')
 
 
-def get_returns_binance(tickers, freq, start_ts, end_ts):
+def download_returns_binance(tickers, freq, start_ts, end_ts):
     prices = {x: binance.get_close(x, freq, start_ts=start_ts, end_ts=end_ts) for x in tickers}
     prices = pd.DataFrame.from_dict(prices, orient='columns')
     prices = prices.reindex(pd.date_range(prices.index[0], prices.index[-1], freq=freq))
